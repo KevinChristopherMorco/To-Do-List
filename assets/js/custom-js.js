46b74dc5aa496b84473dd8e6b968540451cffed2
Kevin Christopher Morco <christophermorco02@gmail.com>
2024-06-11 01:04:38 +0800

    added sort

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -42,14 +42,32 @@ const addTask = () => {
     const convertDate = new Date(taskDate.value)
     const convertTaskDate = `${convertDate.toLocaleString('en-PH', { weekday: 'long' })}, ${convertDate.getDate()} ${convertDate.toLocaleString('en-PH', { month: 'short' })}`
 
+    let taskTier
+    switch (taskPriority.value) {
+        case 'Urgent':
+            taskTier = 1
+            break;
+
+        case 'Important':
+            taskTier = 2
+            break;
+
+        case 'Non-urgent':
+            taskTier = 3
+            break;
+
+        default:
+            break;
+    }
+
     const items = {
         'id': id,
         'taskName': taskName.value,
         'taskDate': convertTaskDate,
         'taskPriority': taskPriority.value,
+        'taskTier': taskTier,
         'taskStatus': 'pending'
     }
-
 
     storageItems.push(items)
     localStorage.setItem('taskDetails', JSON.stringify(storageItems))
@@ -67,14 +85,46 @@ const clear = (elements) => {
     });
 }
 
-const loadItems = () => {
+const loadItems = (e) => {
+    if (localStorage.length === 0) {
+        const emptyTaskTemplate = document.querySelector('#empty__task')
+        const emptyTaskClone = emptyTaskTemplate.content.cloneNode(true)
+        taskWrapper.appendChild(emptyTaskClone)
+        return;
+    }
+
     const pendingTemplate = document.querySelector('#pending__task')
-    JSON.parse(localStorage.getItem('taskDetails')).forEach(element => {
+    const storageItems = JSON.parse(localStorage.getItem('taskDetails'))
+
+    const check = sort(e) === undefined ? storageItems.sort((a, b) => b.taskTier - a.taskTier) : sort(e)
+
+    check.forEach(element => {
         const pendingClone = pendingTemplate.content.cloneNode(true)
-        console.log(element)
         pendingClone.querySelector('.task__name').textContent = element.taskName
         pendingClone.querySelector('.task__date').textContent = element.taskDate
         pendingClone.querySelector('.task__priority').innerHTML = `<span></span>${element.taskPriority}`
+        pendingClone.querySelector('.task__date').textContent = element.taskDate
+        pendingClone.querySelector('.task__buttons .primary__btn').addEventListener('click', finishTask)
+        pendingClone.querySelector('.task__buttons .danger__btn').addEventListener('click', archiveTask)
+
+        const priorityColor = pendingClone.querySelector('.task__priority')
+
+        switch (priorityColor.textContent) {
+            case 'Urgent':
+                priorityColor.classList.add('urgent')
+                break;
+
+            case 'Important':
+                priorityColor.classList.add('important')
+                break;
+
+            case 'Non-urgent':
+                priorityColor.classList.add('non-urgent')
+                break;
+            default:
+                throw new Error('No available color for the status')
+                break;
+        }
         taskWrapper.appendChild(pendingClone)
     })
 }
@@ -85,4 +135,64 @@ const modalState = (isOpen) => {
     console.log(isOpen === true ? 'open' : 'close')
 }
 
-console.log(JSON.parse(localStorage.getItem('taskDetails')))
+const sortType = document.querySelector('.sort__container')
+
+const sortItemType = () => {
+    clearCards()
+    const storageItems = JSON.parse(localStorage.getItem('taskDetails'))
+    const typeValue = taskWrapper.querySelector('#sort-type')
+    switch (typeValue.value) {
+        case 'name':
+            return storageItems.sort((a, b) => a.taskName.localeCompare(b.taskName))
+        case 'priority':
+            return storageItems.sort((a, b) => a.taskTier - b.taskTier)
+        case 'date':
+            return storageItems.sort((a, b) => new Date(a.taskDate) - new Date(b.taskDate))
+        default:
+            break;
+    }
+}
+
+const sortItemOrder = () => {
+    clearCards()
+    const orderValue = taskWrapper.querySelector('#sort-order')
+    switch (orderValue.value) {
+        case 'ascending':
+            return sortItemType()
+            break;
+        // return element.sort((a, b) => b.taskName - a.taskName)
+
+        case 'descending':
+            return sortItemType().reverse()
+            break;
+
+        // return element.sort((a, b) => a.taskTier - b.taskTier)
+        default:
+            break;
+    }
+}
+
+const sort = (e) => {
+    return sortItemOrder()
+}
+
+const clearCards = () => {
+    const cards = taskWrapper.querySelectorAll('.task__card')
+    cards.forEach(card => {
+        card.remove()
+    })
+}
+
+sortType.addEventListener('change', (e) => loadItems(e))
+
+
+const archiveTask = () => {
+    console.log('archived')
+}
+
+const finishTask = () => {
+    console.log('finished')
+}
+
+
+
